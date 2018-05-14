FeedForward fixed, next is backprop

source/function.py
@@ -18,8 +18,8 @@ def tanh_derivative(x):
 
 
 def mse(y, yT):
-    return np.sum((y - yT)**2)
+    return (y - yT)**2/2
 
 
 def mse_derivative(y, yT):
-    return -2 * (y - yT)
+    return (yT - y)

source/main.py
@@ -4,25 +4,23 @@ from neural_network import *
 def main():
 
 	# XOR data set
-	inp = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-	outp = np.array([0, 1, 1, 0])
+	inp =[[1, 0, 1]]
+	
+	outp = [0]
 
+	
 	nn = NeuralNetwork()
 
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
-	nn.add(Layer(2, 2))
+	nn.add(Layer(2, 3))
+	nn.add(Layer(1, 2))
 
-	nn.train(inp, outp, 0.01, 100, 1)
+	nn.train(inp, outp, 0.01, 10, 1)
+	
+
+	#test_set = np.array([[0, 1], [1, 1], [1, 1], [1, 0]])
+	#prediction = nn.predict(test_set)
+	
+	#print(prediction)
 
 if __name__ == '__main__':
 	main()

source/neural_network.py
@@ -18,34 +18,41 @@ class NeuralNetwork(object):
 
 	def feedforward(self, input):
 		self.layers[0] = Layer(num_of_neurons=len(input), outputs=input)
-		for k in xrange(1, self.len):
-			self.layers[k].z = np.dot(self.layers[k].weights, self.layers[k - 1].outputs) + np.transpose(self.layers[k].biases)
-			self.layers[k].outputs = sigmoid(self.layers[k].z)
+		for k in range(1, self.len):
+			self.layers[k].z = np.dot(self.layers[k].weights, np.transpose(self.layers[k - 1].outputs))
+			self.layers[k].z = np.add(self.layers[k].z, self.layers[k].biases)
+
+			self.layers[k].outputs = (self.layers[k].z)
+			#print("<{}> * <{}> + <{}> = {}\n\n".format(self.layers[k].weights, self.layers[k - 1].outputs , np.transpose(self.layers[k].biases), self.layers[k].z))
 	
 	# Returns a tuple (gctw, gctb) wich are
 	# The gradients of cost function with respect to weight and biases
 	# gctw and gctb are numpy arrays
 	def backpropagation(self, output):
-		gctw = [np.zeros((self.layers[i].weights.shape)) for i in xrange(1, self.len)]
-		gctb = [np.zeros((self.layers[i].biases.shape)) for i in xrange(1, self.len)]
+		gctw = [np.zeros((self.layers[i].weights.shape)) for i in range(1, self.len)]
+		gctb = [np.zeros((self.layers[i].biases.shape)) for i in range(1, self.len)]
+		#for i in gctw:
+		#print("OI {}".format(i))
 
 		# Backward pass, delta from last layer
 		# Calculates partial derivative of the cost with respect to the input from layer L
-		delta = mse(self.layers[-1].outputs, output) * sigmoid_derivative(self.layers[-1].z)
+		delta = mse_derivative(self.layers[-1].outputs, output) * sigmoid_derivative(self.layers[-1].z)
+		#ERRO AQUIIIIIIIIIIII< DIMENSAO ERRADA
 		gctb[-1] = delta
-		gctw[-1] = np.dot(delta, np.transpose(self.layers[-2].outputs))		
+		gctw[-1] = np.dot(delta, self.layers[-2].outputs)		
 
 		# Putting the back in the backpropagation
-		for l in reversed(xrange(1, self.len - 1)):
+		for l in reversed(range(1, self.len - 1)):
 			# Calculates partial derivative of the cost with respect to the input from layer l
-			delta = np.dot((np.transpose(self.layers[l+1].weights) * delta), sigmoid_derivative(self.layers[l].z))
+			delta = np.dot(np.transpose(self.layers[l+1].weights)*delta, sigmoid_derivative(self.layers[l].z))
 			gctb[l-1] = delta
-			gctw[l-1] = np.dot(self.layers[l].outputs, delta)			
+			gctw[l-1] = np.dot(delta, np.transpose(self.layers[l].outputs))			
+			#print("[{}]".format(gctw[l-1]))
 
 		return (gctw, gctb)
 
 	def gradientdescent(self, gctw, gctb, lr):
-		for l in xrange(1, self.len):
+		for l in range(1, self.len):
 			self.layers[l].weights = self.layers[l].weights - lr*gctw[l-1]
 			self.layers[l].biases = self.layers[l].biases - lr*gctb[l-1]
 
@@ -53,9 +60,23 @@ class NeuralNetwork(object):
 	# Computes the backpropagation every single input (SLOW)
 	# Using stochastic gradient descent (Every input we update)
 	def train(self, training_input, training_output, lr=0.1, epochs=10, iterations=100):
-		for i in xrange(epochs):
+		for i in range(epochs):
+			cost = 0
 			for _input, _output in zip(training_input, training_output):
 				self.feedforward(_input)
-				gctw, gctb = self.backpropagation(_output)
-				self.gradientdescent(gctw, gctb, lr)
-			print("Cost for epoch[{}] = {}".format(i, mse(self.layers[-1].outputs, _output)))
+				cost = mse(self.layers[-1].outputs, _output)
+				#print(cost)
+				#gctw, gctb = self.backpropagation(_output)
+				#self.gradientdescent(gctw, gctb, lr)
+				
+			#print("Cost for epoch[{}] = {}".format(i, cost))
+
+	# Returns an np.array with the predictions given a test_set
+	def predict(self, test_set):
+		prediction = np.zeros(len(test_set))
+
+		for i in range(len(test_set)):
+			self.feedforward(test_set[i])
+			prediction[i] = self.layers[-1].outputs
+
+		return prediction